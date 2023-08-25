76b5f9c57e10b4175ae0b6bbb3a61308bee370bb
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-08-25 15:41:11 +1200

    First working version of uplink controller with message unpacking

--- myriotaUplinkWebhook/Controllers/UplinkController.cs
+++ myriotaUplinkWebhook/Controllers/UplinkController.cs
@@ -1,33 +1,63 @@
-using Microsoft.AspNetCore.Mvc;
 
-namespace myriotaUplinkWebhook.Controllers
+// Copyright (c) August 2023, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Azure.Storage.Queues;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+
+namespace devMobile.IoT.myriotaAzureIoTConnector.myriota.UplinkWebhook.Controllers
 {
-    [ApiController]
     [Route("[controller]")]
+    [ApiController]
     public class UplinkController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
+        private readonly Models.ApplicationSettings _applicationSettings;
         private readonly ILogger<UplinkController> _logger;
+        private readonly QueueServiceClient _queueServiceClient;
 
-        public UplinkController(ILogger<UplinkController> logger)
+        public UplinkController(IOptions<Models.ApplicationSettings> applicationSettings, QueueServiceClient queueServiceClient, ILogger<UplinkController> logger)
         {
+            _applicationSettings = applicationSettings.Value;
+            _queueServiceClient = queueServiceClient;
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Models.UplinkPayloadWebDto payloadWeb)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            _logger.LogInformation("SendAsync queue name:{QueueName}", _applicationSettings.QueueName);
+
+            QueueClient queueClient = _queueServiceClient.GetQueueClient(_applicationSettings.QueueName);
+
+            var serializeOptions = new JsonSerializerOptions
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+
+            await queueClient.SendMessageAsync(Convert.ToBase64String(JsonSerializer.SerializeToUtf8Bytes(payloadWeb, serializeOptions)));
+
+            return this.Ok();
         }
     }
 }
---
+++ myriotaUplinkWebhook/Models/UplinkPayloads.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) August 2023, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Used new Visual Studio feature "Edit->Paste Special->Paste JSON as Clases" functionality
+//
+//---------------------------------------------------------------------------------
+
+namespace devMobile.IoT.myriotaAzureIoTConnector.myriota.UplinkWebhook.Models
+{
+    public class UplinkPayloadWebDto
+    {
+        public string EndpointRef { get; set; }
+        public long Timestamp { get; set; } 
+        public string Data { get; set; } // Embedded JSON
+        public string Id { get; set; }
+        public string CertificateUrl { get; set; }
+        public string Signature { get; set; }
+    }
+}
