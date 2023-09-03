325584aa757dcb6e156de97cf2af87bf6774d5f1
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-09-04 11:11:25 +1200

    First payload formatter, the default one

---
+++ PayloadFormatters/default.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class FormatterUplink : PayloadFormatter.IFormatterUplink
+{
+    public JObject Evaluate(IDictionary<string, string> properties, string application, string terminalId, DateTime timestamp, JObject payloadJson, string payloadText, byte[] payloadBytes)
+    {
+        JObject telemetryEvent = new JObject();
+
+        telemetryEvent.Add("ASCII", payloadText);
+        telemetryEvent.Add("Bits", BitConverter.ToString(payloadBytes));
+
+        return telemetryEvent;
+    }
+}
