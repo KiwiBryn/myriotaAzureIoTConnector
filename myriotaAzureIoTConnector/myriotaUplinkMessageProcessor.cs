e3a4cf5d4def5951bceb9c23a61b3653d4c7b379
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-10-01 21:07:53 +1300

    Removed nolonger necessary using

--- myriotaAzureIoTConnector/myriotaUplinkMessageProcessor.cs
+++ myriotaAzureIoTConnector/myriotaUplinkMessageProcessor.cs
@@ -16,17 +16,11 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Net;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 using Microsoft.Azure.Devices.Client;
 using Microsoft.Azure.Devices.Client.Exceptions;
-using Microsoft.Azure.Devices.Provisioning.Client;
-using Microsoft.Azure.Devices.Provisioning.Client.PlugAndPlay;
-using Microsoft.Azure.Devices.Provisioning.Client.Transport;
-using Microsoft.Azure.Devices.Shared;
 using Microsoft.Azure.Functions.Worker;
 
 using Microsoft.Extensions.Caching.Memory;
