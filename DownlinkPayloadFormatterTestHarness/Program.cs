7c1f84438a985fa5d21eaa7c8ef4afb3c8ee907c
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-10-04 11:54:50 +1300

    Stubbed out downlink payload formatter so a rebase is less painful.

---
+++ DownlinkPayloadFormatterTestHarness/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DownlinkPayloadFormatterTestHarness
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
