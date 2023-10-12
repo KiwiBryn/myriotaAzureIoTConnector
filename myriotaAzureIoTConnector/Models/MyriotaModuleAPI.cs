abf876e83e8c2da01cd0b5d315dc07bc6af9f4a0
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-10-12 14:12:13 +1300

    First cut of the myriotaAPI send control message method

--- myriotaAzureIoTConnector/Models/MyriotaModuleAPI.cs
+++ myriotaAzureIoTConnector/Models/MyriotaModuleAPI.cs
@@ -38,4 +38,18 @@ namespace devMobile.IoT.MyriotaAzureIoTConnector.Connector.Models
         public List<Item> Items { get; set; }
         public string NextItem { get; set; } = string.Empty;
     }
+
+    public class ControlMessageSendRequest
+    {
+        public string ModuleId { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ControlMessageSendResponse
+    {
+        public string Id { get; set; }
+        public string ModuleId { get; set; }
+        public string Status { get; set; }
+    }
+
 }
