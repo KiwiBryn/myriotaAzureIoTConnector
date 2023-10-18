ef1c9c8831de6b3b1f56c7a71f8d02ea49aa2124
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2023-10-18 15:30:02 +1300

    Added fields for passing round payload formatters

--- myriotaAzureIoTConnector/Models/DeviceContext.cs
+++ myriotaAzureIoTConnector/Models/DeviceContext.cs
@@ -20,14 +20,18 @@ using Microsoft.Azure.Devices.Client;
 
 namespace devMobile.IoT.MyriotaAzureIoTConnector.Connector.Models
 {
-    public class DeviceConnectionContext
-    {
-        public string TerminalId { get; set; } = string.Empty;
+   public class DeviceConnectionContext
+   {
+      public string TerminalId { get; set; } = string.Empty;
 
-        public DeviceClient DeviceClient { get; set;}
+      public string ModuleType { get; set; } = string.Empty;
 
-        public string ModuleType { get; set; } = string.Empty;
+      public string PayloadFormatterUplink { get; set; } = string.Empty;
 
-        public Dictionary<string, string> Attibutes { get; set; }
-    }
+      public string PayloadFormatterDownlink { get; set; } = string.Empty;
+
+      public Dictionary<string, string> Attibutes { get; set; }
+
+      public DeviceClient DeviceClient { get; set; }
+   }
 }
