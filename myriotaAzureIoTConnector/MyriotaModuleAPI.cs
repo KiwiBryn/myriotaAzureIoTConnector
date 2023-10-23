8ee21fe081330f95f67e4d7271a6d52829bf0e66
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2023-10-23 19:31:20 +1300

    Chantged way message body generated as kept on getting 400s

--- myriotaAzureIoTConnector/MyriotaModuleAPI.cs
+++ myriotaAzureIoTConnector/MyriotaModuleAPI.cs
@@ -15,6 +15,7 @@
 //---------------------------------------------------------------------------------
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -26,89 +27,89 @@ using RestSharp;
 
 namespace devMobile.IoT.MyriotaAzureIoTConnector.Connector
 {
-    internal class MyriotaModuleAPI : IMyriotaModuleAPI
-    {
-        private readonly ILogger<MyriotaModuleAPI> _logger;
-        private readonly Models.MyriotaSettings _myiotaSettings;
+   internal class MyriotaModuleAPI : IMyriotaModuleAPI
+   {
+      private readonly ILogger<MyriotaModuleAPI> _logger;
+      private readonly Models.MyriotaSettings _myiotaSettings;
 
-        public MyriotaModuleAPI(ILogger<MyriotaModuleAPI> logger, IOptions<Models.MyriotaSettings> myiotaSettings)
-        {
-            _logger = logger;
-            _myiotaSettings = myiotaSettings.Value;
-        }
+      public MyriotaModuleAPI(ILogger<MyriotaModuleAPI> logger, IOptions<Models.MyriotaSettings> myiotaSettings)
+      {
+         _logger = logger;
+         _myiotaSettings = myiotaSettings.Value;
+      }
 
-        public async Task<Models.Item> GetAsync(string TerminalId, CancellationToken cancellationToken)
-        {
-            RestClientOptions restClientOptions = new RestClientOptions()
-            {
-                BaseUrl = new Uri(_myiotaSettings.BaseUrl),
-                ThrowOnAnyError = true,
-            };
+      public async Task<Models.Item> GetAsync(string TerminalId, CancellationToken cancellationToken)
+      {
+         RestClientOptions restClientOptions = new RestClientOptions()
+         {
+            BaseUrl = new Uri(_myiotaSettings.BaseUrl),
+            ThrowOnAnyError = true,
+         };
 
+         using (RestClient client = new RestClient(restClientOptions))
+         {
+            RestRequest request = new RestRequest($"v1/modules/{TerminalId}?Destinations=false");
+
+            request.AddHeader("Authorization", _myiotaSettings.ApiToken);
+
+            Models.ModulesResponse response = await client.GetAsync<Models.ModulesResponse>(request, cancellationToken);
+
+            return response.Items[0];
+         }
+      }
+
+      public async Task<ICollection<Models.Item>> ListAsync(CancellationToken cancellationToken)
+      {
+         RestClientOptions restClientOptions = new RestClientOptions()
+         {
+            BaseUrl = new Uri(_myiotaSettings.BaseUrl),
+            ThrowOnAnyError = true,
+         };
+
+         // Need to rewrite this to handle paging
+         using (RestClient client = new RestClient(restClientOptions))
+         {
+            RestRequest request = new RestRequest("v1/modules?Destinations=false");
+
+            request.AddHeader("Authorization", _myiotaSettings.ApiToken);
+
+            Models.ModulesResponse response = await client.GetAsync<Models.ModulesResponse>(request, cancellationToken);
+
+            return response.Items;
+         }
+      }
+
+      public async Task<string> SendAsync(string terminalId, byte[] payload, CancellationToken cancellationToken = default)
+      {
+         Models.ControlMessageSendRequest message = new Models.ControlMessageSendRequest()
+         {
+            ModuleId = terminalId,
+            Message = Convert.ToHexString(payload),
+         };
+
+         RestClientOptions restClientOptions = new RestClientOptions()
+         {
+            BaseUrl = new Uri(_myiotaSettings.BaseUrl),
+            ThrowOnAnyError = true,
+         };
+
+         if (_myiotaSettings.DownlinkEnabled)
+         {
             using (RestClient client = new RestClient(restClientOptions))
             {
-                RestRequest request = new RestRequest($"v1/modules/{TerminalId}?Destinations=false");
+               RestRequest request = new RestRequest("v1/control-messages/", Method.Post);
 
-                request.AddHeader("Authorization", _myiotaSettings.ApiToken);
+               request.AddJsonBody(JsonSerializer.Serialize(message));
 
-                Models.ModulesResponse response = await client.GetAsync<Models.ModulesResponse>(request, cancellationToken);
+               request.AddHeader("Authorization", _myiotaSettings.ApiToken);
 
-                return response.Items[0];
+               Models.ControlMessageSendResponse sendResponse = await client.PostAsync<Models.ControlMessageSendResponse>(request, cancellationToken);
+
+               return sendResponse.Id;
             }
-        }
+         }
 
-        public async Task<ICollection<Models.Item>> ListAsync(CancellationToken cancellationToken)
-        {
-            RestClientOptions restClientOptions = new RestClientOptions()
-            {
-                BaseUrl = new Uri(_myiotaSettings.BaseUrl),
-                ThrowOnAnyError = true,
-            };
-
-            // Need to rewrite this to handle paging
-            using (RestClient client = new RestClient(restClientOptions))
-            {
-                RestRequest request = new RestRequest("v1/modules?Destinations=false");
-
-                request.AddHeader("Authorization", _myiotaSettings.ApiToken);
-
-                Models.ModulesResponse response = await client.GetAsync<Models.ModulesResponse>(request, cancellationToken);
-
-                return response.Items;
-            }
-        }
-
-        public async Task<String> SendAsync(string terminalId, byte[] payload, CancellationToken cancellationToken = default)
-        {
-            Models.ControlMessageSendRequest sendRequest = new Models.ControlMessageSendRequest()
-            {
-                ModuleId = terminalId,
-                Message = Convert.ToBase64String(payload),
-            };
-
-            RestClientOptions restClientOptions = new RestClientOptions()
-            {
-                BaseUrl = new Uri(_myiotaSettings.BaseUrl),
-                ThrowOnAnyError = true,
-            };
-
-            if (_myiotaSettings.DownlinkEnabled)
-            {
-                using (RestClient client = new RestClient(restClientOptions))
-                {
-                    RestRequest request = new RestRequest("v1/control-messages/", Method.Post);
-
-                    request.AddBody(sendRequest);
-
-                    request.AddHeader("Authorization", _myiotaSettings.ApiToken);
-
-                    Models.ControlMessageSendResponse sendResponse = await client.PostAsync<Models.ControlMessageSendResponse>(request, cancellationToken);
-
-                    return sendResponse.Id;
-                }
-            }
-
-            return string.Empty;
-        }
-    }
+         return string.Empty;
+      }
+   }
 }
