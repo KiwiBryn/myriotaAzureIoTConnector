7587172983e4936d1bf4b61875dc9502c416e355
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2023-10-30 13:44:09 +1300

    First cut of Azure IoT Central downlink sample formatters

---
+++ PayloadFormatters/Downlink/LightsOffOn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class FormatterDownlink : PayloadFormatter.IFormatterDownlink
+{
+   public byte[] Evaluate(IDictionary<string, string> properties, string terminalId, JObject payloadJson, byte[] payloadBytes)
+   {
+      bool? light = payloadJson.Value<bool?>("Light");
+
+      if (!light.HasValue)
+      {
+         return new byte[] { };
+      }
+
+      return new byte[] { Convert.ToByte(light.Value) };
+   }
+}
