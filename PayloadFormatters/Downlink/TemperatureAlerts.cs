1803b9c3e5024cf01b4a5ddb005a25fd2f5535df
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2023-11-02 17:04:39 +1300

    Added formatyter for min + max temperature alerting

---
+++ PayloadFormatters/Downlink/TemperatureAlerts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+public class FormatterDownlink : PayloadFormatter.IFormatterDownlink
+{
+   public byte[] Evaluate(IDictionary<string, string> properties, string terminalId, JObject payloadJson, byte[] payloadBytes)
+   {
+      // Case sensitive ?
+      double? minimum = payloadJson.Value<double?>("Minimum");
+
+      double? maximum = payloadJson.Value<double?>("Maximum");
+
+      if (!minimum.HasValue || !maximum.HasValue)
+      {
+         return new byte[] { };
+      }
+
+      byte[] result = new byte[17];
+
+      result[0] = 3;
+
+      BitConverter.GetBytes(minimum.Value).CopyTo(result, 1);
+
+      BitConverter.GetBytes(maximum.Value).CopyTo(result, 9);
+
+      return result;
+   }
+}
