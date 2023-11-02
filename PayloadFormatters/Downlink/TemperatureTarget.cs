80ed54889621bedb928074964a858606b9050a20
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2023-11-02 17:05:05 +1300

    Added formatter for handling desired temperature setting

---
+++ PayloadFormatters/Downlink/TemperatureTarget.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class FormatterDownlink : PayloadFormatter.IFormatterDownlink
+{
+   public byte[] Evaluate(IDictionary<string, string> properties, string terminalId, JObject payloadJson, byte[] payloadBytes)
+   {
+      double? temperature = payloadJson.Value<double?>("TemperatureTarget");
+
+      if (!temperature.HasValue)
+      {
+         return new byte[] { };
+      }
+
+      byte[] result = new byte[9];
+
+      result[0] = 2;
+
+      BitConverter.GetBytes(temperature.Value).CopyTo(result, 1);
+
+      return result;
+   }
+}
