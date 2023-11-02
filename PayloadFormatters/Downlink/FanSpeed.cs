7f226cce53db49cbb475fb860e3fa6f05fed46ad
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2023-11-02 17:01:31 +1300

    Updated name to better reflect what formatter does

--- PayloadFormatters/Downlink/FanSpeed.cs
+++ PayloadFormatters/Downlink/FanSpeed.cs
@@ -8,13 +8,13 @@ public class FormatterDownlink : PayloadFormatter.IFormatterDownlink
 {
    public byte[] Evaluate(IDictionary<string, string> properties, string terminalId, JObject payloadJson, byte[] payloadBytes)
    {
-      byte? status = payloadJson.Value<byte?>("FanStatus");
+      byte? status = payloadJson.Value<byte?>("FanSpeed");
 
       if (!status.HasValue)
       {
          return new byte[] { };
       }
 
-      return new byte[] { status.Value };
+      return new byte[] { 1, status.Value };
    }
 }
