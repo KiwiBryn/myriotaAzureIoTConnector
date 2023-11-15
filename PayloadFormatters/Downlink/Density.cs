c9b271c5259bab2d921759998e51cca07bc3b6c6
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2023-11-15 22:03:04 +1300

    Modified handler so the Front, Medium and Back values extracted correctly. BEWARE CASE SENSITIVE

--- PayloadFormatters/Downlink/Density.cs
+++ PayloadFormatters/Downlink/Density.cs
@@ -6,8 +6,31 @@ using Newtonsoft.Json.Linq;
 
 public class FormatterDownlink : PayloadFormatter.IFormatterDownlink
 {
-    public byte[] Evaluate(IDictionary<string, string> properties, string terminalId, JObject payloadJson, byte[] payloadBytes)
-    {
-        return payloadBytes;
-    }
+   public byte[] Evaluate(IDictionary<string, string> properties, string terminalId, JObject payloadJson, byte[] payloadBytes)
+   {
+      float? front = payloadJson.Value<float>("Front");
+      float? middle = payloadJson.Value<float>("Middle");
+      float? back = payloadJson.Value<float>("Back");
+
+      byte[] result = new byte[13];
+
+      result[0] = 5;
+
+      if (front.HasValue)
+      {
+         BitConverter.GetBytes(front.Value).CopyTo(result, 1);
+      }
+
+      if (middle.HasValue)
+      {
+         BitConverter.GetBytes(middle.Value).CopyTo(result, 5);
+      }
+
+      if (back.HasValue)
+      {
+         BitConverter.GetBytes(back.Value).CopyTo(result, 9);
+      }
+
+      return result;
+   }
 }
