43f1d94f517501decbff971c8846e323174d57d4
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2023-11-15 18:17:31 +1300

    Added formatters for 3D vector & hashtable so can test some more downlink message types. Need to expand hastable formatter to pick info from JSOn message payload

---
+++ PayloadFormatters/Downlink/Acceleration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class FormatterDownlink : PayloadFormatter.IFormatterDownlink
+{
+   public byte[] Evaluate(IDictionary<string, string> properties, string terminalId, JObject payloadJson, byte[] payloadBytes)
+   {
+      float x = payloadJson.Value<float>("x");
+      float y = payloadJson.Value<float>("y");
+      float z = payloadJson.Value<float>("z");
+
+      byte[] result = new byte[13];
+
+      result[0] = 4;
+
+      BitConverter.GetBytes(x).CopyTo(result, 1);
+      BitConverter.GetBytes(y).CopyTo(result, 5);
+      BitConverter.GetBytes(x).CopyTo(result, 9);
+
+      return result;
+   }
+}
