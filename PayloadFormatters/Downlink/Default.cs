022b79a7bd1a746ada6a8e5ea093e3c8884fd2c5
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2023-12-03 14:31:07 +1300

    Sorted out capitalisation

---
+++ PayloadFormatters/Downlink/Default.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class FormatterDownlink : PayloadFormatter.IFormatterDownlink
+{
+    public byte[] Evaluate(string terminalId, JObject payloadJson, byte[] payloadBytes)
+    {
+        return payloadBytes;
+    }
+}
