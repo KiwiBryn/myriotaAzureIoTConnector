8d6d2a5aad9e3cf1d452c62bc58ebbed0408a422
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-08-22 12:43:30 +1200

    Updated code with global usings and trimmed (c) notice

--- myriotaAzureIoTConnector/MyriotaModuleAPI.cs
+++ myriotaAzureIoTConnector/MyriotaModuleAPI.cs
@@ -1,29 +1,5 @@
-﻿// Copyright (c) October 2023, devMobile Software
+﻿// Copyright (c) October 2023, devMobile Software, MIT License
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-//---------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
-
-using Microsoft.Extensions.Options;
-
-using RestSharp;
-
-
 namespace devMobile.IoT.MyriotaAzureIoTConnector.Connector
 {
    internal class MyriotaModuleAPI(IOptions<Models.MyriotaSettings> myiotaSettings) : IMyriotaModuleAPI
--- myriotaAzureIoTConnector/StartupService.cs
+++ myriotaAzureIoTConnector/StartupService.cs
@@ -1,28 +1,5 @@
-﻿// Copyright (c) October 2023, devMobile Software
+﻿// Copyright (c) October 2023, devMobile Software, MIT License
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-//---------------------------------------------------------------------------------
-using System;
-using System.Threading;
-using System.Threading.Tasks;
-
-using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
-
 [assembly: FunctionsStartup(typeof(devMobile.IoT.MyriotaAzureIoTConnector.Connector.StartUpService))]
 namespace devMobile.IoT.MyriotaAzureIoTConnector.Connector
 {
