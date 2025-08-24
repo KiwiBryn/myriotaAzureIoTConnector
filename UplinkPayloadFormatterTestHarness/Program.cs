7315b407a372837e5a6930ac13686bd43d5652f5
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-08-25 10:50:05 +1200

    Chopped back (c) notice and add global usings. Preparing for Newtonsoft replacement with System.Text.Json

--- UplinkPayloadFormatterTestHarness/Program.cs
+++ UplinkPayloadFormatterTestHarness/Program.cs
@@ -1,29 +1,11 @@
-﻿// Copyright (c) October 2023, devMobile Software
+﻿// Copyright (c) October 2023, devMobile Software, MIT License
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-//---------------------------------------------------------------------------------
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
 namespace devMobile.IoT.myriotaAzureIoTConnector.UplinkPayloadFormatterTestHarness
 {
-    using System;
-    using System.Globalization;
-
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
-
-    using CommandLine;
-    using CSScriptLib;
-
     internal class Program
     {
         static void Main(string[] args)
