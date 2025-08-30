b82c38e7af20d32add2331a4336cf2e8255895e4
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-08-30 16:35:28 +1200

    First workinbg version of formatter need to test with the updated downlink formatters

--- DownlinkPayloadFormatterTestHarness/Program.cs
+++ DownlinkPayloadFormatterTestHarness/Program.cs
@@ -1,116 +1,84 @@
 ﻿// Copyright (c) October 2023, devMobile Software, MIT License
 //
-using Newtonsoft.Json.Linq;
+namespace devMobile.IoT.myriotaAzureIoTConnector.DownlinkPayloadFormatterTestHarness;
 
 
-namespace devMobile.IoT.myriotaAzureIoTConnector.DownlinkPayloadFormatterTestHarness
+internal class Program
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args)
-              .WithParsed(ApplicationCore)
-              .WithNotParsed(HandleParseError);
+   static void Main(string[] args)
+   {
+      CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args)
+        .WithParsed(ApplicationCore)
+        .WithNotParsed(HandleParseError);
 
-            Console.WriteLine("Press <enter> to exit");
-            Console.ReadLine();
-        }
+      Console.WriteLine("Press <enter> to exit");
+      Console.ReadLine();
+   }
 
-        private static void HandleParseError(IEnumerable<Error> errors)
-        {
-            if (errors.IsVersion())
-            {
-                Console.WriteLine("Version Request");
-                return;
-            }
+   private static void HandleParseError(IEnumerable<Error> errors)
+   {
+      if (errors.IsVersion())
+      {
+         Console.WriteLine("Version Request");
+         return;
+      }
 
-            if (errors.IsHelp())
-            {
-                Console.WriteLine("Help Request");
-                return;
-            }
-            Console.WriteLine("Parser Fail");
-        }
+      if (errors.IsHelp())
+      {
+         Console.WriteLine("Help Request");
+         return;
+      }
+      Console.WriteLine("Parser Fail");
+   }
 
-        private static void ApplicationCore(CommandLineOptions options)
-        {
-            Dictionary<string, string> properties = new Dictionary<string, string>();
+   private static void ApplicationCore(CommandLineOptions options)
+   {
+      Console.WriteLine($"Downlink formatter file:{options.FormatterPath}");
 
-            Console.WriteLine($"Downlink formatter file:{options.FormatterPath}");
+      PayloadFormatter.IFormatterDownlink evaluatorDownlink;
+      try
+      {
+         evaluatorDownlink = CSScript.Evaluator.LoadFile<PayloadFormatter.IFormatterDownlink>(options.FormatterPath);
+      }
+      catch (CompilerException cex)
+      {
+         Console.Write($"Loading or compiling file:{options.FormatterPath} failed Exception:{cex}");
+         return;
+      }
 
-            PayloadFormatter.IFormatterDownlink evaluatorDownlink;
-            try
-            {
-                evaluatorDownlink = CSScript.Evaluator.LoadFile<PayloadFormatter.IFormatterDownlink>(options.FormatterPath);
-            }
-            catch (CSScriptLib.CompilerException cex)
-            {
-                Console.Write($"Loading or compiling file:{options.FormatterPath} failed Exception:{cex}");
-                return;
-            }
+      byte[] payloadHex;
 
-            byte[] payloadHex = { };
+      try
+      {
+         payloadHex = Convert.FromHexString(options.PayloadHex);
+      }
+      catch (FormatException fex)
+      {
+         Console.WriteLine("Convert.FromHexString failed:{0}", fex.Message);
+         return;
+      }
 
-            if (!string.IsNullOrWhiteSpace(options.PayloadHex))
-            {
-                try
-                {
-                    payloadHex = Convert.FromHexString(options.PayloadHex);
-                }
-                catch (FormatException fex)
-                {
-                    Console.WriteLine("Convert.FromHexString failed:{0}", fex.Message);
-                    return;
-                }
-            }
+      JsonObject? payloadJson;
 
-            JObject? payloadJson = null  ;
+      try
+      {
+         payloadJson = JsonNode.Parse(File.ReadAllText(options.JsonPayloadPath)) as JsonObject;
+      }
+      catch (FormatException fex)
+      {
+         Console.WriteLine($"Downlink payload file invalid format {options.JsonPayloadPath} not found:{fex}");
+         return;
+      }
 
-            if (!string.IsNullOrWhiteSpace(options.JsonPayloadPath))
-            {
-                Console.WriteLine($"Downlink JSON payloadFilename:{options.JsonPayloadPath}");
+      try
+      {
+         byte[] payloadBytes = evaluatorDownlink.Evaluate(options.TerminalId, options.MethodName, payloadJson, payloadHex);
 
-                try
-                {
-                    payloadJson = JObject.Parse(File.ReadAllText(options.JsonPayloadPath));
-                }
-                catch (DirectoryNotFoundException dex)
-                {
-                    Console.WriteLine($"Downlink payload filename directory {options.JsonPayloadPath} not found:{dex}");
-                    return;
-                }
-                catch (FileNotFoundException fnfex)
-                {
-                    Console.WriteLine($"Downlink payload filename {options.JsonPayloadPath} not found:{fnfex}");
-                    return;
-                }
-                catch (FormatException fex)
-                {
-                    Console.WriteLine($"Downlink payload file invalid format {options.JsonPayloadPath} not found:{fex}");
-                    return;
-                }
-            }
-
-            byte[] payloadBytes;
-            try
-            {
-                payloadBytes = evaluatorDownlink.Evaluate(options.TerminalId, options.MethodName, payloadJson, payloadHex);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"evalulatorDownlink.Evaluate failed Exception:{ex}");
-                return;
-            }
-
-            Console.WriteLine("Properties:");
-            foreach (var property in properties)
-            {
-                Console.WriteLine($"{property.Key}:{property.Value}");
-            }
-            Console.WriteLine("");
-
-            Console.WriteLine($"Downlink payload:{Convert.ToHexString(payloadBytes)} Bytes:{payloadBytes.Length}");
-        }
-    }
+         Console.WriteLine($"Downlink payload:{Convert.ToHexString(payloadBytes)} Bytes:{payloadBytes.Length}");
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"evalulatorDownlink.Evaluate failed Exception:{ex}");
+      }
+   }
 }
