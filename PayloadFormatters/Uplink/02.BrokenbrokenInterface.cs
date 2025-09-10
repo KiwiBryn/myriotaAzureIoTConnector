19ee8901c94a430bfb347ee96dc3849c0680d369
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-09-10 13:33:56 +1200

    Fixed up Newtonsoft to System.Text.Json issues

--- PayloadFormatters/Uplink/02.BrokenbrokenInterface.cs
+++ PayloadFormatters/Uplink/02.BrokenbrokenInterface.cs
@@ -2,11 +2,11 @@
 //
 using System;
 using System.Collections.Generic;
-
+using System.Text.Json.Nodes;
 
 public class FormatterUplink : PayloadFormatter.IFormatterUplink
 {
-    public JObject Evaluate(string terminalId, IDictionary<string, string> properties, DateTime timestamp, byte payloadBytes)
+    public JsonObject Evaluate(string terminalId, IDictionary<string, string> properties, DateTime timestamp, byte payloadBytes)
     {
         JsonObject telemetryEvent = new JsonObject();
 
--- PayloadFormatters/Uplink/default.cs
+++ PayloadFormatters/Uplink/default.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿// Copyright (c) October 2023, devMobile Software, MIT License
+//
+using System;
 using System.Collections.Generic;
 using System.Globalization;
-
 using System.Text.Json.Nodes;
+
+
 public class FormatterUplink : PayloadFormatter.IFormatterUplink
 {
    public JsonObject Evaluate(string terminalId, IDictionary<string, string> properties, DateTime timestamp, byte[] payloadBytes)
