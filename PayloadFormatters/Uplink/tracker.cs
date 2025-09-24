4075f4996a3cbb8866efe62671b7e873567643ea
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-09-25 11:26:14 +1200

    Fixed up payload formatter to unpack new message format

--- PayloadFormatters/Uplink/tracker.cs
+++ PayloadFormatters/Uplink/tracker.cs
@@ -1,14 +1,20 @@
 ﻿/*
 Copyright (c) August 2025, devMobile Software, MIT License
 
-myriota tracker payload format
-
+// Location entry with latitude, longitude, and timestamp. Values are little endian.
 typedef struct {
-  uint16_t sequence_number;
   int32_t latitude;   // scaled by 1e7, e.g. -891234567 (south 89.1234567)
   int32_t longitude;  // scaled by 1e7, e.g. 1791234567 (east 179.1234567)
-  uint32_t time;      // epoch timestamp of last fix
-} __attribute__((packed)) tracker_message; 
+  uint32_t time;      // epoch timestamp of location record
+} __attribute__((packed)) location_t;
+
+// Format of the messages to be transmitted.
+typedef struct {
+  uint16_t sequence_number;  // Sequence number of the message
+  uint8_t location_count;    // Number of location entries in the below array
+  location_t locations[LOCATIONS_PER_MESSAGE];  // Array of location entries
+
+} __attribute__((packed)) tracker_message;
 
 */
 using System;
@@ -19,37 +25,70 @@ using System.Text.Json.Nodes;
 
 public class FormatterUplink : PayloadFormatter.IFormatterUplink
 {
-    public JsonObject Evaluate(string terminalId, IDictionary<string, string> properties, DateTime timestamp, byte[] payloadBytes)
-    {
+   public JsonObject Evaluate(string terminalId, IDictionary<string, string> properties, DateTime timestamp, byte[] payloadBytes)
+   {
       JsonObject telemetryEvent = new JsonObject();
 
-        if (payloadBytes is null)
-        {
-            return telemetryEvent;
-        }
+      if (payloadBytes is null)
+      {
+         return telemetryEvent;
+      }
 
-        telemetryEvent.Add("SequenceNumber", BitConverter.ToUInt16(payloadBytes));
+      telemetryEvent.Add("SequenceNumber", BitConverter.ToUInt16(payloadBytes));
 
-        JsonObject location = new JsonObject();
+      double latitude = 0.0;
+      double longitude = 0.0;
+      double altitude = 0.0;
+      DateTime fixAtUtc = DateTime.UtcNow;
 
-        double latitude = BitConverter.ToInt32(payloadBytes, 2) / 10000000.0;
-        location.Add("lat", latitude);
+      byte locationCount = payloadBytes[2];
 
-        double longitude = BitConverter.ToInt32(payloadBytes, 6) / 10000000.0;
-        location.Add("lon", longitude);
+      JsonArray locations = new JsonArray();
 
-        location.Add("alt", 0);
+      for (int index = 0; index < locationCount; index++)
+      {
+         JsonObject location = new JsonObject();
 
-        telemetryEvent.Add("DeviceLocation", location);
+         int offset = (index * 12) + 3;
 
-        UInt32 packetimestamp = BitConverter.ToUInt32(payloadBytes, 10);
+         latitude = BitConverter.ToInt32(payloadBytes, offset) / 10000000.0;
+         location.Add("lat", latitude);
 
-        DateTime fixAtUtc = DateTime.UnixEpoch.AddSeconds(packetimestamp);
+         longitude = BitConverter.ToInt32(payloadBytes, offset + 4) / 10000000.0;
+         location.Add("lon", longitude);
 
-        telemetryEvent.Add("FixAtUtc", fixAtUtc);
+         location.Add("alt", altitude);
 
-        properties.Add("iothub-creation-time-utc", fixAtUtc.ToString("s", CultureInfo.InvariantCulture));
+         UInt32 packetimestamp = BitConverter.ToUInt32(payloadBytes, offset + 8);
 
-        return telemetryEvent;
-    }
+         fixAtUtc = DateTime.UnixEpoch.AddSeconds(packetimestamp);
+
+         location.Add("FixAtUtc", fixAtUtc);
+
+         locations.Add(location);
+      }
+
+      telemetryEvent.Add("Locations", locations);
+
+      JsonObject deviceLocationLatest = new JsonObject();
+
+      deviceLocationLatest.Add("lat", latitude);
+      deviceLocationLatest.Add("lon", longitude);
+      deviceLocationLatest.Add("alt", altitude);
+
+      telemetryEvent.Add("DeviceLocation", deviceLocationLatest);
+      
+
+      JsonObject trackingLatest = new JsonObject();
+      trackingLatest.Add("lat", latitude);
+      trackingLatest.Add("lon", longitude);
+      trackingLatest.Add("alt", altitude);
+
+      telemetryEvent.Add("Tracking", trackingLatest);
+      telemetryEvent.Add("FixAtUtc", fixAtUtc);
+
+      properties.Add("iothub-creation-time-utc", fixAtUtc.ToString("s", CultureInfo.InvariantCulture));
+
+      return telemetryEvent;
+   }
 }
