98402fbca8b539aca612ca46dc7d707d3a0a6969
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2026-01-29 13:54:58 +1300

    Updated to new namespace format

--- IPayloadFormatter/PayloadFormatter.cs
+++ IPayloadFormatter/PayloadFormatter.cs
@@ -1,14 +1,13 @@
 ﻿// Copyright (c) August 2023, devMobile Software, MIT License
 //
-namespace PayloadFormatter // Additional namespace for shortening interface when usage in formatter code
-{
-   public interface IFormatterUplink
-   {
-      public JsonObject Evaluate(string terminalId, IDictionary<string, string> properties, DateTime timestamp, byte[] payloadBytes);
-   }
+namespace PayloadFormatter; // Additional namespace for shortening interface when usage in formatter code
 
-   public interface IFormatterDownlink
-   {
-      public byte[] Evaluate(string terminalId, string methodName, JsonObject payloadJson, byte[] payloadBytes);
-   }
+public interface IFormatterUplink
+{
+   public JsonObject Evaluate(string terminalId, IDictionary<string, string> properties, DateTime timestamp, byte[] payloadBytes);
+}
+
+public interface IFormatterDownlink
+{
+   public byte[] Evaluate(string terminalId, string methodName, JsonObject payloadJson, byte[] payloadBytes);
 }
