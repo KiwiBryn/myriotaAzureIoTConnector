19735e8cb0c30dc1c3fc8724e7bfb01df923b628
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2026-01-29 13:54:18 +1300

    Added default values

--- DownlinkPayloadFormatterTestHarness/CommandLineOptions.cs
+++ DownlinkPayloadFormatterTestHarness/CommandLineOptions.cs
@@ -1,22 +1,21 @@
 ﻿// Copyright (c) October 2023, devMobile Software, MIT License
 //
-namespace devMobile.IoT.myriotaAzureIoTConnector.DownlinkPayloadFormatterTestHarness
+namespace devMobile.IoT.myriotaAzureIoTConnector.DownlinkPayloadFormatterTestHarness;
+
+public class CommandLineOptions
 {
-    public class CommandLineOptions
-    {
-        [Option('f', "Formatter", Required = true, HelpText = "payload formatter path")]
-        public string FormatterPath { get; set; }
+   [Option('f', "Formatter", Required = true, HelpText = "payload formatter path")]
+   public string FormatterPath { get; set; } = string.Empty;
 
-        [Option('t', "TerminalId", Required = true, HelpText = "Terminal unique identitifer")]
-        public string TerminalId { get; set; }
+   [Option('t', "TerminalId", Required = true, HelpText = "Terminal unique identitifer")]
+   public string TerminalId { get; set; } = string.Empty;
 
-        [Option('m', "MethodName", Required = true, HelpText = "Method name")]
-        public string MethodName { get; set; }
+   [Option('m', "MethodName", Required = true, HelpText = "Method name")]
+   public string MethodName { get; set; } = string.Empty;
 
-        [Option('j', "JSONFile", Required = false, HelpText = "JSON payload file path")]
-        public string JsonPayloadPath { get; set; }
+   [Option('j', "JSONFile", Required = false, HelpText = "JSON payload file path")]
+   public string JsonPayloadPath { get; set; } = string.Empty;
 
-        [Option('h', "PayloadHex", Required = false, HelpText = "Hex payload")]
-        public string PayloadHex { get; set; }
-    }
+   [Option('h', "PayloadHex", Required = false, HelpText = "Hex payload")]
+   public string PayloadHex { get; set; } = string.Empty;
 }
