7b6527f2acfcb67a8cfa358647ec55071b334067
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2026-01-29 14:30:18 +1300

    Reasonable defaults plus updated namespace style

--- UplinkPayloadFormatterTestHarness/CommandLineOptions.cs
+++ UplinkPayloadFormatterTestHarness/CommandLineOptions.cs
@@ -1,19 +1,18 @@
 ﻿// Copyright (c) October 2023, devMobile Software, MIT License
 //
-namespace devMobile.IoT.myriotaAzureIoTConnector.UplinkPayloadFormatterTestHarness
-{ 
-    public class CommandLineOptions
-    {
-        [Option('f', "Formatter", Required = true, HelpText = "payload formatter path")]
-        public string FormatterPath{ get; set; }
+namespace devMobile.IoT.myriotaAzureIoTConnector.UplinkPayloadFormatterTestHarness;
 
-        [Option('t', "TerminalId", Required = true, HelpText = "Terminal unique identitifer")]
-        public string TerminalId { get; set; }
+public class CommandLineOptions
+{
+   [Option('f', "Formatter", Required = true, HelpText = "payload formatter path")]
+   public string FormatterPath { get; set; } = string.Empty;
 
-        [Option('T', "Timestamp", Required = false, HelpText = "Payload packet UTC timestamp(optional, defaults to current)")]
-        public DateTime? TimeStamp { get; set; }
+   [Option('t', "TerminalId", Required = true, HelpText = "Terminal unique identitifer")]
+   public string TerminalId { get; set; } = string.Empty;
 
-        [Option('h', "PayloadHex", Required = true, HelpText = "Hex payload")]
-        public string PayloadHex { get; set; }
-    }
+   [Option('T', "Timestamp", Required = false, HelpText = "Payload packet UTC timestamp(optional, defaults to current)")]
+   public DateTime? TimeStamp { get; set; }
+
+   [Option('h', "PayloadHex", Required = true, HelpText = "Hex payload")]
+   public string PayloadHex { get; set; } = string.Empty;
 }
